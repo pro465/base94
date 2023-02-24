8516b85ca6121906cbb8560bb526b8360896cfea
Proloy Mishra <namitapro0987@gmail.com>
2023-02-24 10:56:09 +0530

    generalized the algorithm to any set of 1-byte digits

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,121 @@
-pub fn encode(src: &[u8]) -> Vec<u8> {
-    let mut res = Vec::new();
-    for c in src.chunks(9) {
-        conv(c, &mut res, 256, 94, 1, 11);
-    }
-    res.iter_mut().for_each(|i| *i += 33);
-    res
-}
+pub const BASE94: &[u8;94] = br##"!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"##;
+pub const BASE95: &[u8;95] = br##" !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"##;
 
-pub fn decode(src: &[u8]) -> Vec<u8> {
+pub fn encode(src: &[u8], allowed_bytes: &[u8]) -> Vec<u8> {
+    assert!((0..allowed_bytes.len() - 1).all(|i| allowed_bytes[i] < allowed_bytes[i + 1]));
     let mut res = Vec::new();
+    let base = allowed_bytes.len() as u128;
+    let r = max(base).1;
+    let m = 1 << r;
     let mut i = 0;
+    let mut acc = 0;
+    let mut max = 1;
 
     while i < src.len() {
-        let mut c = [0; 11];
-        let mut len = 0;
-        while i < src.len() && len < 11 {
-            if (33..127).contains(&src[i]) {
-                c[len] = src[i] - 33;
-                len += 1;
-            }
-            i += 1;
+        acc += max * u128::from(src[i]);
+        max <<= 8;
+        if max >= m {
+            encode_internal(&mut res, allowed_bytes, acc % m, m);
+            acc >>= r;
+            max >>= r;
         }
-        conv(&c[..len], &mut res, 94, 256, 255, 9);
+        i += 1;
     }
+
+    encode_internal(&mut res, allowed_bytes, acc, max);
+
     res
 }
 
-fn conv(
-    batch: &[u8],
-    res: &mut Vec<u8>,
-    from_base: u128,
-    to_base: u128,
-    min: u128,
-    max_len: usize,
-) {
+pub fn decode(src: &[u8], allowed_bytes: &[u8]) -> Vec<u8> {
+    assert!((0..allowed_bytes.len() - 1).all(|i| allowed_bytes[i] < allowed_bytes[i + 1]));
+    let mut res = Vec::new();
+    let base = allowed_bytes.len() as u128;
+    let r = max(base).0;
+    let m = base.pow(r);
     let mut i = 0;
+    let mut acc = 0;
     let mut max = 1;
-    for &d in batch.iter().rev() {
-        i *= from_base;
-        i += d as u128;
-        max *= from_base;
-    }
-    max -= 1;
-    for _ in 0..max_len {
-        if max < min {
-            break;
+    let mut bits_used = 0;
+    let mut bits = 0;
+
+    while i < src.len() {
+        let a = max
+            * match allowed_bytes.binary_search(&src[i]) {
+                Ok(x) => x as u128,
+                _ => {
+                    i += 1;
+                    continue;
+                }
+            };
+        acc += a;
+        max *= base;
+        if max >= m {
+            (bits_used, bits) = decode_internal(&mut res, acc, m, bits_used, bits);
+            acc = 0;
+            max = 1;
         }
-        res.push((i % to_base) as u8);
-        i /= to_base;
+        i += 1;
+    }
+
+    decode_internal(&mut res, acc, max, bits_used, bits);
+
+    res
+}
+
+fn log2(i: u128) -> u32 {
+    let mut j = 0;
+    while i >= 1 << (j + 1) {
+        j += 1;
+    }
+    j
+}
+
+fn max(base: u128) -> (u32, u32) {
+    let (mut m, mut r) = (f64::INFINITY, (0, 0));
+    let mut a: u128 = 1;
+    let mut i = 0;
+
+    while let Some(new) = a.checked_mul(base) {
+        a = new;
+        i += 1;
+        let j = log2(a);
+        let score = (8 * i) as f64 / j as f64;
+        if score < m {
+            m = score;
+            r = (i, j);
+        }
+    }
+
+    r
+}
+
+fn decode_internal(
+    res: &mut Vec<u8>,
+    mut acc: u128,
+    mut max: u128,
+    prev: u32,
+    prev_bits: u8,
+) -> (u32, u8) {
+    max <<= prev;
+    acc <<= prev;
+    acc |= prev_bits as u128;
+
+    while max > 255 {
+        res.push(acc as u8);
+
+        acc /= 256;
+        max /= 256;
+    }
+
+    (log2(max), acc as u8)
+}
+
+fn encode_internal(res: &mut Vec<u8>, allowed_bytes: &[u8], mut acc: u128, mut max: u128) {
+    let to_base = allowed_bytes.len() as u128;
+    while max > 0 {
+        res.push(allowed_bytes[(acc % to_base) as usize]);
+        acc /= to_base;
         max /= to_base;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,7 @@ fn main() {
     let mut args = std::env::args().skip(1);
     let flag = args.next().unwrap_or_else(|| help());
 
-    /*    let (mut m, mut r) = (f64::INFINITY, (0, 0));
-        for i in 0..19 {
-            let a: u128 = 94_u128.pow(i + 1);
-            let mut j = 0;
-            while a >= 1 << (j + 1) {
-                j += 1;
-            }
-            let score = (8 * i + 8) as f64 / j as f64;
-            if score < m {
-                m = score;
-                r = (i + 1, j);
-            }
-        }
-
-        dbg!(r, m);
-    */
-
-    let conv: fn(_) -> _ = match flag.as_str() {
+    let conv: fn(_, _) -> _ = match flag.as_str() {
         "-e" => base94::encode,
         "-d" => base94::decode as _,
         _ => help(),
@@ -32,7 +15,11 @@ fn main() {
             .expect("could not canonicalize argument"),
     )
     .expect("could not read file");
-    fs::write(args.next().unwrap_or_else(|| help()), &conv(&src)).expect("could not write to file");
+    fs::write(
+        args.next().unwrap_or_else(|| help()),
+        &conv(&src, base94::BASE95),
+    )
+    .expect("could not write to file");
 }
 
 fn help() -> ! {
@@ -49,41 +36,3 @@ FLAG:
     );
     std::process::exit(-1);
 }
-/*
-fn encode(src: Vec<u8>, mut dest: impl Write) {
-    for c in src.chunks(9) {
-        let mut res = conv(c, 256, 94, 0);
-        res.truncate(11);
-        res.iter_mut().for_each(|i| *i += 33);
-        dest.write_all(&res).unwrap();
-    }
-}
-
-fn decode(mut src: Vec<u8>, mut dest: impl Write) {
-    src.retain(|i| (33..127).contains(i));
-
-    for c in src.chunks_mut(11) {
-        c.iter_mut().for_each(|i| *i -= 33);
-        let mut res = conv(c, 94, 256, 254);
-        res.truncate(9);
-        dest.write_all(&res).unwrap();
-    }
-}
-
-fn conv(batch: &[u8], from_base: u128, to_base: u128, min: u128) -> Vec<u8> {
-    let mut res = Vec::new();
-    let mut i = 0;
-    let mut max = 1;
-    for &d in batch.iter().rev() {
-        i *= from_base;
-        i += d as u128;
-        max *= from_base;
-    }
-    max -= 1;
-    while max > min {
-        res.push((i % to_base) as u8);
-        i /= to_base;
-        max /= to_base;
-    }
-    res
-}*/
